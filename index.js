56a0dab5ec3ce365d2afe51c3b24cba612e125f2
Sergey Chivirda <si.chivirda@gmail.com>
2023-07-14 16:22:02 +0300

    get book list

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import puppeteer from 'puppeteer'
 
 const svarogSite = 'https://fantlab.ru/work7754'
+const books = getList()
 
 async function getList() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(svarogSite)
 
-  const element = await page.waitForSelector('.dots > span > a')
-
-  console.log(element)
-
+  const books = await page.$$eval('.dots > span > a', books => {
+    return books.map(book => book.textContent)
+                .filter(book => !book.startsWith('«'))
+  })
+  return books
 }
-
-getList()
